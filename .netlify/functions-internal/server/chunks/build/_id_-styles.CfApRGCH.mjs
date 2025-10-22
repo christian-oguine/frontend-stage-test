12d33945f5a220838a5c394d0ffa67e9fc5f88f1
Oguine Christian <christian.oguine@student.howest.be>
2025-10-22 23:43:42 +0200

    chore: prepare for Netlify deploy

---
+++ .netlify/functions-internal/server/chunks/build/_id_-styles.CfApRGCH.mjs
@@ -0,0 +1,8 @@
+const _id__vue_vue_type_style_index_0_scoped_7ede92c2_lang = ".stat-card[data-v-7ede92c2]{background-color:#ffffff0a;border-color:#ffffff1a;border-radius:1rem;border-width:1px;padding:1rem}.stat-label[data-v-7ede92c2]{color:#fff9;font-family:Poppins,sans-serif;font-size:.75rem;line-height:1rem}.stat-value[data-v-7ede92c2]{font-family:Cinzel Decorative,serif;font-size:1.125rem;line-height:1.75rem;margin-top:.25rem}.panel[data-v-7ede92c2]{background-color:#ffffff0a;border-color:#ffffff1a;border-radius:1rem;border-width:1px;padding:1.25rem}.panel-title[data-v-7ede92c2]{font-family:Cinzel Decorative,serif;font-size:1.25rem;line-height:1.75rem;margin-bottom:.5rem}.list[data-v-7ede92c2]>:not([hidden])~:not([hidden]){--tw-space-y-reverse:0;margin-bottom:calc(.25rem*var(--tw-space-y-reverse));margin-top:calc(.25rem*(1 - var(--tw-space-y-reverse)))}.list[data-v-7ede92c2]{color:#ffffffd9;font-family:Poppins,sans-serif;font-size:.875rem;line-height:1.25rem;padding-left:.25rem}.fact[data-v-7ede92c2]{display:flex;gap:.5rem}.fact .label[data-v-7ede92c2]{color:#fff9}";
+
+const _id_Styles_CfApRGCH = [
+  _id__vue_vue_type_style_index_0_scoped_7ede92c2_lang
+];
+
+export { _id_Styles_CfApRGCH as default };
+//# sourceMappingURL=_id_-styles.CfApRGCH.mjs.map
